dfbb7ad1dfaedbf084d434485d81e581e5928ea0
jc <jchorzempa@posteo.org>
2022-12-16 19:04:09 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,18 @@
+class Player {
+    constructor(name, species, hp, mp) {
+        this.name = name;
+        this.species = species;
+        this.hp = hp;
+        this.mp = mp;
+    }
+    getName() {
+        return this.name;
+    }
+    getHp() {
+        return this.hp;
+    }
+    getmp() {
+        return this.mp;
+    }
+
+}
